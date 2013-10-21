37c40ac448e795e968f6af810d40f8ccf53f3cba
Russ Wirtz <rwirtz@zipwhip.com>
2013-10-21 13:01:47 -0700

    Add activeMQ dependencies

--- src/main/java/com/zipwhip/website/controllers/ComponentController.java
+++ src/main/java/com/zipwhip/website/controllers/ComponentController.java
@@ -3,6 +3,8 @@
  */
 package com.zipwhip.website.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,12 +17,26 @@ import java.util.Map;
 @Controller
 public class ComponentController {
 
-    @RequestMapping(value = "/memcached", method = RequestMethod.GET)
-    @ResponseBody
-    public Map<String, Object> evaluate(HttpServletRequest request) {
-        String query = request.getParameter("query");
+    @Autowired
+    JmsTemplate jmsTemplate;
 
-        return Collections.singletonMap("query", (Object) query);
+    @RequestMapping(value = "/jms/send", method = RequestMethod.GET)
+    @ResponseBody
+    public Map<String, Object> sendJms(HttpServletRequest request) {
+        String path = request.getParameter("path");
+        String message = request.getParameter("message");
+
+        jmsTemplate.convertAndSend(path, message);
+
+        return Collections.singletonMap("sent", (Object)Boolean.TRUE);
+    }
+
+    @RequestMapping(value = "/jms/receive", method = RequestMethod.GET)
+    @ResponseBody
+    public Map<String, Object> receiveJms(HttpServletRequest request) {
+        String path = request.getParameter("path");
+
+        return Collections.singletonMap(path, jmsTemplate.receiveAndConvert(path));
     }
 
 }
